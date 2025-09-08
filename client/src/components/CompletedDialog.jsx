3c5d8e59463fd181b5ca58788fac5d099fbd0cac
abhix079 <abhishek788k@gmail.com>
2025-09-09 00:34:18 +0530

    minor changes

--- client/src/components/CompletedDialog.jsx
+++ client/src/components/CompletedDialog.jsx
@@ -1,31 +1,39 @@
 import styles from "../styles/Dialog.module.css";
 import { FaUser } from "react-icons/fa";
 
-export default function CompletedDialog({ closeDialog }) {
+export default function CompletedDialog({ closeDialog, patient }) {
   return (
     <div className={styles.dialogBox}>
       <div className={styles.dialogHeader}>
         <h2>Patient Details</h2>
-        <button className={styles.btn} onClick={closeDialog}>Close</button>
+        <button className={styles.btn} onClick={closeDialog}>
+          Close
+        </button>
       </div>
+
       <div className={styles.patientDetail}>
         <p className={styles.dialogHeadings}>Personal Details</p>
-        <div className={styles.personalDetail}> 
+        <div className={styles.personalDetail}>
           <div className={styles.leftSide}>
-            <p><strong>Patient ID : </strong> P001</p>
-            <p><strong>Name : </strong> Aditya Kumar</p>
-            <p><strong>Age : </strong>22</p>
-            <p><strong>Gender : </strong>Male</p>
-            <p><strong>Address : </strong>Raptinagar Phase-4, Gorakhpur, 273003</p>
+            <p><strong>Patient ID : </strong> {patient.patientId}</p>
+            <p><strong>Name : </strong> {patient.name}</p>
+            <p><strong>Age : </strong> {patient.age}</p>
+            <p><strong>Gender : </strong> {patient.gender}</p>
+            <p><strong>Address : </strong> {patient.address}</p>
           </div>
           <div className={styles.rightSide}><FaUser size={60} /></div>
         </div>
+
         <p className={styles.dialogHeadings}>Medical Details</p>
         <div className={styles.medicalDetail}>
-          <p><strong>Referred By : </strong> Dr. Ankit</p>
-          <p><strong>Symptom / Reason : </strong>Cold and Fever</p>
+          <p><strong>Referred By : </strong> 
+            {patient.referredBy?.firstName} {patient.referredBy?.lastName}
+          </p>
+          <p><strong>Symptom / Reason : </strong> {patient.reason}</p>
+          {patient.prescription && (
+            <p><strong>Prescription : </strong> {patient.prescription}</p>
+          )}
         </div>
-    
       </div>
     </div>
   );
