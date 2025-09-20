75823d4f598ee00e9f070789796428032bc8084c
abhix079 <abhishek788k@gmail.com>
2025-09-21 01:06:29 +0530

    upd:index

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,9 +2,11 @@ import express from "express";
 import { doctorLogin,staffLogin,adminLogin } from "../controllers/authController.js";
 
 const router = express.Router();
-
+//route for doctor login
 router.post("/doctor", doctorLogin);
+//route for staff login
 router.post("/staff", staffLogin);
+//route for admin lofin
 router.post("/admin", adminLogin);
 
 export default router; 
